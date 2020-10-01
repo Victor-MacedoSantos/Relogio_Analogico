91adc5a4662d1b78556db93774c98fa2a1234d8d
Victor Macêdo <victormacedo.santos.00@gmail.com>
2020-10-01 02:17:20 -0300

    Relogio Analogico

---
+++ script.js
@@ -0,0 +1,16 @@
+const secDiv = document.getElementById('sec');
+const minDiv = document.getElementById('min');
+const hrDiv = document.getElementById('hr');
+
+setInterval(clock, 1000);
+
+function clock (){
+    let date = new Date();
+    let sec = date.getSeconds() / 60;
+    let min = date.getMinutes() / 60;
+    let hr = date.getHours() / 12;
+
+    secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
+    minDiv.style.transform = "rotate(" + (min * 360) + "deg)";
+    hrDiv.style.transform = "rotate(" + (hr * 360) + "deg)";
+}
